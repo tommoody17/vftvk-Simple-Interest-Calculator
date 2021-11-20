b4c324ced2d17d6bd278c464595568bca761169b
tommoody17 <94228262+tommoody17@users.noreply.github.com>
2021-11-20 20:07:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate= document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal*years*rate / 100;
+    var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    // add results to the "result" inner HTML in an easily readable format
+    // sample given on the exercise referred to a var "amount" which i changed to interest
+    document.getElementById("result").innerHTML="\<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 }
 function updateRate()
 {
@@ -12,4 +15,15 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 
 }
-       
+function checkData()
+{
+    var principal = document.getElementById("principal").value;
+    // check if principal value is zero or negative
+    if(principal <= 0){
+        alert("Enter a positive number");
+        // bring user attention to the principal input
+        principal.focus();
+        return false;
+    }
+    return true;
+}
